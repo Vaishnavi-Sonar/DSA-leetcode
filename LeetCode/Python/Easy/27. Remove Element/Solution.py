93540dca2ab2b17db94b20df989dfa8c7d4a2397
Vaishnavi-Sonar <vaishnavisonar246@gmail.com>
2026-07-22 19:46:34 +0530

    Add Easy Python Solution from LeetCode

---
+++ LeetCode/Python/Easy/27. Remove Element/Solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+
+        k = 0
+        for n in nums:
+            if n != val:
+                nums[k] = n
+                k += 1
+        return k
+
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        
