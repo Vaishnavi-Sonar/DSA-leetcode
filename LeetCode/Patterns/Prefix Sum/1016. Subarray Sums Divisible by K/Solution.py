70308086e21ea2e9cd05a24d0a8b52d4501e0805
Vaishnavi-Sonar <vaishnavisonar246@gmail.com>
2026-07-22 19:44:48 +0530

    Add Python Solution to Patterns/Prefix Sum

---
+++ LeetCode/Patterns/Prefix Sum/1016. Subarray Sums Divisible by K/Solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def subarraysDivByK(self, nums, k):
+
+        curr = 0
+        count = 0
+        seen = {0:1}
+
+        for n in nums:
+            curr += n
+            rem = curr % k
+            if rem in seen:
+                count += seen[rem]
+            seen[rem] = seen.get(rem, 0) + 1
+        
+        return count
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
