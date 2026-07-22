5c053eb553ec0b45b1239b11ab2ea62e0846593d
Vaishnavi-Sonar <vaishnavisonar246@gmail.com>
2026-07-22 19:48:46 +0530

    Add Python Solution to Patterns/Two Pointers

---
+++ LeetCode/Patterns/Two Pointers/283. Move Zeroes/Solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[k], nums[i] = nums[i], nums[k]
+                k += 1
+                    
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
