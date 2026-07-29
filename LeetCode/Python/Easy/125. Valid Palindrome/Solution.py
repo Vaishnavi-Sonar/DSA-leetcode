822aa13a90ab2852c1111d890c0a8c3ae0b0dbd3
Vaishnavi-Sonar <vaishnavisonar246@gmail.com>
2026-07-29 15:42:41 +0530

    Add Easy Python Solution from LeetCode

---
+++ LeetCode/Python/Easy/125. Valid Palindrome/Solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, s):
+
+        s = ''.join(c.lower() for c in s if c.isalnum())
+        return s == s[::-1]
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
