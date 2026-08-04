c96e713e583c5c27aa22e390097ba688878eb663
Vaishnavi-Sonar <vaishnavisonar246@gmail.com>
2026-08-04 20:30:35 +0530

    Add Python Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/1019. Squares of a Sorted Array/Solution.py
+++ LeetCode/Patterns/Two Pointers/1019. Squares of a Sorted Array/Solution.py
@@ -1,8 +1,27 @@
 class Solution(object):
     def sortedSquares(self, nums):
 
-        return sorted(list(n*n for n in nums))
+        # return sorted(list(n*n for n in nums))
+
+        left = 0
+        right = len(nums) - 1
+
+        ans = [0]*len(nums)
+        i = len(nums) - 1
+
+        while left <= right:
+            if nums[left]**2 > nums[right]**2:
+                ans[i] = nums[left]**2
+                left += 1
+            else:
+                ans[i] = nums[right]**2
+                right -= 1
+            
+            i -= 1
         
+        return ans
+            
+
         """
         :type nums: List[int]
         :rtype: List[int]
